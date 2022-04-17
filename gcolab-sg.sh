b0de0015667095c0d48ee92956a77acabb1d2209
MahirAlam <103423298+MahirAlam@users.noreply.github.com>
2022-04-17 18:12:46 +0600

    Update gcolab-sg.sh

--- gcolab-sg.sh
+++ gcolab-sg.sh
@@ -2,10 +2,16 @@
 # Install XRDP
 # Before get xmr coin for free
 # Google Colab
+#sudo apt update
+#sudo apt install screen
+#screen -R xmr
+#wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
+#tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
+#cd xmrig-6.16.4
+#./xmrig -o stratum+tcp://randomxmonero.eu-west.nicehash.com:3380 -u 35qheUaRktAzmvTyBdpzih4moF5E3yDb6K -p x -k --nicehash -a rx/0
+
 sudo apt update
-sudo apt install screen
-screen -R xmr
-wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
-tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
-cd xmrig-6.16.4
-./xmrig -o stratum+tcp://randomxmonero.eu-west.nicehash.com:3380 -u 35qheUaRktAzmvTyBdpzih4moF5E3yDb6K -p x -k --nicehash -a rx/0
+sudo apt install jq
+mydata = !curl -O /content.json 'https://raw.githubusercontent.com/MahirAlam/rdps/main/config.json';
+touch process.json
+jq -n --arg mydata "$mydata" '$mydata' > process.json
