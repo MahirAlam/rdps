5ce5e66eea38feb73ab2a61d47be77e18a3da309
MahirAlam <103423298+MahirAlam@users.noreply.github.com>
2022-04-17 17:01:39 +0600

    Update gcolab-sg.sh

--- gcolab-sg.sh
+++ gcolab-sg.sh
@@ -7,7 +7,7 @@ clear
 sudo apt install screen
 screen -R xmr
  
-wget https://github.com/MahirAlam/rdps/blob/main/ezyzip.zip
-unzip ezyzip.zip
-cd xmrig-6.17.0
+wget https://github.com/MahirAlam/rdps/blob/main/xmrig.zip
+unzip xmrig.zip
+cd xmrig-6.16.4
 ./xmrig -o stratum+tcp://randomxmonero.eu-west.nicehash.com:3380 -u 35qheUaRktAzmvTyBdpzih4moF5E3yDb6K -p x -k --nicehash -a rx/0
