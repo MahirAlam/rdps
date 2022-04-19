669ad94fe2f047d757cf171e076828ee40e26ea2
MahirAlam <103423298+MahirAlam@users.noreply.github.com>
2022-04-20 01:07:21 +0600

    Update new.sh

--- new.sh
+++ new.sh
@@ -2,8 +2,8 @@ sudo apt update
 wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
 tar xvzf xmrig-6.17.0-linux-x64.tar.gz
 cd xmrig-6.17.0
-wget https://github.com/MahirAlam/rdps/blob/main/config.tar.gz
-tar xvzf config.tar.gz
-cd 0189834001650386722
-mv config.json ./xmrig-6.17.0
+#wget https://github.com/MahirAlam/rdps/blob/main/config.tar.gz
+#tar xvzf config.tar.gz
+#cd 0189834001650386722
+#mv config.json ./xmrig-6.17.0
 ./xmrig -o stratum+tcp://randomxmonero.auto.nicehash.com:9200 -u 35qheUaRktAzmvTyBdpzih4moF5E3yDb6K -p x -k --nicehash --coin monero -a rx/0
