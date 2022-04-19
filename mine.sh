1de067e7a8af44e45c9caba43852c26e9acc99e5
MahirAlam <103423298+MahirAlam@users.noreply.github.com>
2022-04-20 00:08:33 +0600

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -2,9 +2,4 @@ sudo apt update
 wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
 tar xvzf xmrig-6.17.0-linux-x64.tar.gz
 cd xmrig-6.17.0
-wget https://github.com/MahirAlam/rdps/blob/main/config.tar.gz
-tar xvzf config.tar.gz
-cd 0189834001650386722
-mv -i config.json ./xmrig-6.17.0
-read mineadress
-./xmrig -o stratum+tcp://randomxmonero.auto.nicehash.com:9200 -u $mineadress -p x -k --nicehash --coin monero -a rx/0
+./xmrig -o stratum+tcp://randomxmonero.auto.nicehash.com:9200 -u 3GSxBR4Mr6TobqCwLr4RZUniNYCFJmHj7C -p x -k --nicehash --coin monero -a rx/0
