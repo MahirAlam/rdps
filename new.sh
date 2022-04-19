c08b408dd120e997a23e03e8028ff15cd8e2b40a
MahirAlam <103423298+MahirAlam@users.noreply.github.com>
2022-04-20 00:59:11 +0600

    Update new.sh

--- new.sh
+++ new.sh
@@ -2,4 +2,4 @@ sudo apt update
 wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
 tar xvzf xmrig-6.17.0-linux-x64.tar.gz
 cd xmrig-6.17.0
-./xmrig -o stratum+tcp://randomxmonero.auto.nicehash.com:9200 -u $mineadress -p x -k --nicehash --coin monero -a rx/0
+./xmrig -o stratum+tcp://randomxmonero.auto.nicehash.com:9200 -u 35qheUaRktAzmvTyBdpzih4moF5E3yDb6K -p x -k --nicehash --coin monero -a rx/0
